09f0ecc8f673e7833dcd1805934ffdcf9008e4d5
Aika <caigerim7@gmail.com>
2020-12-27 11:17:08 -0500

    added petal

--- start/reverse_test.py
+++ start/reverse_test.py
@@ -14,7 +14,6 @@ def test_reverse():
 
 test_reverse()
 
-
 def reverse2(s: str) -> str:
 
     return ''.join([s[i] for i in range(len(s)-1,-1,-1)])
@@ -26,3 +25,40 @@ def test_reverse2():
     assert reverse2("abc") == "cba"
 
 test_reverse2()
+
+# Petals
+import turtle
+
+def petals(fred: turtle):
+    for i in range(5):
+        fred.right(20)
+        fred.circle(100, 90)
+        fred.circle(-100, 90)
+        fred.circle(100, 90)
+        fred.circle(-100, 90)
+
+        fred.left(-20)
+        fred.circle(-100, -90)
+        fred.circle(100, -90)
+        fred.circle(-100, -90)
+        fred.circle(100, -90)
+
+def main():
+    fred = turtle.Turtle()
+    fred.speed(20)
+
+    wn = turtle.Screen()
+
+    fred.up()
+    fred.goto(-200, -200)
+    fred.down()
+
+    petals(fred)
+
+    wn.exitonclick()
+
+main()
+
+
+
+
