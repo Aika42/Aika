c4f0bd86eb542935f639ba63f4f5af3067819163
vlad chub <vlad.chub@gmail.com>
2020-12-27 11:19:46 -0800

    add homework.md, add int_len()

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# Aika
+
+### PyCharm Setup
+
+https://www.jetbrains.com/help/pycharm/choosing-your-testing-framework.html
+
+Open the Settings/Preferences, and under the node Tools, click the page Python Integrated Tools.
+
+On this page, click the Default Test Runner field.
+
+Choose the desired test runner: pytest
+
+You may have a message - "no pytest runner is found in interpreter".
+Click Fix
+
+
+Run your tests:
+
+	Run->run test runner
+	or
+	Ctr-R
 
 
 
+
+
+--------------------
--- start/reverse_test.py
+++ start/reverse_test.py
@@ -1,23 +1,25 @@
-def reverse(s: str) -> str:
+def reverse1(s: str) -> str:
     s1 = []
 
-    i = len(s)-1
+    i = len(s) - 1
     while i > -1:
         s1.append(s[i])
         i = i - 1
-    return ''.join(s1)
+    return "".join(s1)
+
 
 def test_reverse():
-    assert reverse("a") == "a"
-    assert reverse("ab") == "ba"
-    assert reverse("abc") == "cba"
+    assert reverse1("a") == "a"
+    assert reverse1("ab") == "ba"
+    assert reverse1("abc") == "cba"
+
 
 test_reverse()
 
 
 def reverse2(s: str) -> str:
 
-    return ''.join([s[i] for i in range(len(s)-1,-1,-1)])
+    return "".join([s[i] for i in range(len(s) - 1, -1, -1)])
 
 
 def test_reverse2():
@@ -25,4 +27,24 @@ def test_reverse2():
     assert reverse2("ab") == "ba"
     assert reverse2("abc") == "cba"
 
-test_reverse2()
+
+# test_reverse2()
+
+# Write a function that will return the number of digits in an integer.
+# You can NOT use str() to convert int into a string
+# You should use operation  // in a loop (or recursion)
+def int_len(n: int) -> int:
+    # NotImplemented
+    a = 0
+    i = 10
+    # while n > 0:
+    #     NotImplemented
+    #     i+=1
+    return a
+
+
+def test_int_len():
+    assert int_len(1) == 1
+    assert int_len(10) == 2
+    assert int_len(100) == 3
+    assert int_len(1111) == 4
