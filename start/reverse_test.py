d20d16361fd44c6349dd84c24e892027a81cbdca
Aika <caigerim7@gmail.com>
2020-12-26 22:14:51 -0500

    reverse list comprehension

--- start/reverse_test.py
+++ start/reverse_test.py
@@ -7,11 +7,22 @@ def reverse(s: str) -> str:
         i = i - 1
     return ''.join(s1)
 
-
-
 def test_reverse():
     assert reverse("a") == "a"
     assert reverse("ab") == "ba"
     assert reverse("abc") == "cba"
 
 test_reverse()
+
+
+def reverse2(s: str) -> str:
+
+    return ''.join([s[i] for i in range(len(s)-1,-1,-1)])
+
+
+def test_reverse2():
+    assert reverse2("a") == "a"
+    assert reverse2("ab") == "ba"
+    assert reverse2("abc") == "cba"
+
+test_reverse2()
