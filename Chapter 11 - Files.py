3df6739c3676b3b620b2cd9afddd592bb0f421ab
Aika <caigerim7@gmail.com>
2021-01-03 19:34:51 -0500

    test

--- Chapter 11 - Files.py
+++ Chapter 11 - Files.py
@@ -1,4 +1,4 @@
-# 11.9.1
+# 11.9.1 prints out the names of students that have more than six quiz scores
 def studentdata():
 
     f = open('studentdata.txt', 'r')
@@ -11,20 +11,38 @@ def studentdata():
     f.close()
 
 
-# 11.9.2
+# 11.9.2 average grade for each student in file
 def ave_grade():
     f = open('studentdata.txt', 'r')
 
     for line in f:
         xs = line.split()
-        print(xs)
-        #print(','.join(line[1:]))
-
-        #print(xs[0], 'average grade: ', xs[1:]) #sum(xs[1:]))
-        #a = ','.join(xs[1:])
-        #print(a)
+        summ = 0
+        for ch in xs[1:]:
+            summ = summ + int(ch)
+        print(xs[0], summ)
 
     f.close()
 
-ave_grade()
+# 11.9.3 minimum and maximum score for each student
+def scores():
+    f = open('studentdata.txt', 'r')
+
+    for line in f:
+        xs = line.split()
+        maxx = int(xs[1:][0])
+        minn = int(xs[1:][0])
+        for value in xs[1:]:
+            value = int(value)
+            if value > maxx:
+                maxx = value
+            if value < minn:
+                minn = value
+
+        print(xs[0], 'max value: ', maxx, 'min value: ', minn)
+
+    f.close()
+
+
+
 
