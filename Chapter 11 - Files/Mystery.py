d9bd08476c0e975ded40e343eb848a31c12c42d4
Aika <caigerim7@gmail.com>
2021-01-09 09:25:53 -0500

    Mystery cont

--- Chapter 11 - Files/Mystery.py
+++ Chapter 11 - Files/Mystery.py
@@ -1,33 +1,19 @@
 def mystery():
-    # load points
-    points = []
-    with open('Mystery.txt') as f:
-        for l in f:
-            points.append(l.split())
-
-
-    for i in range(len(points)-1):
-        if points[i][0] == 'UP' or points[i][0] == 'DOWN':
-            points[i] = points[i][0]
-        else:
-            points[i] = [int(points[i][0]),int(points[i][1])]
-
-    print(points)
     import turtle
     wn = turtle.Screen()
-    #wn.setworldcoordinates()
+    t = turtle.Turtle()
 
-    fred = turtle.Turtle()
-
-    for i in range(10):
-        print(points[i][0],points[i][1])
-        if points[i] == 'UP':
-            fred.up()
-        if points[i] == 'DOWN':
-            fred.down()
-        else:
-            fred.goto(points[i][0],points[i][1])
-            fred.stamp()
+    # load points
+    with open('Mystery.txt') as f:
+        for l in f:
+            points = l.split()
+            if points[0] == 'UP':
+                t.up()
+            elif points[0] == 'DOWN':
+                t.down()
+            else:
+                t.goto(int(points[0]),int(points[1]))
+                t.stamp()
 
     wn.exitonclick()
 
